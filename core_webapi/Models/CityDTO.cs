4000f37b700eba68b9d1494f84549b875c659804
LEE Toni <toni.lee@soprasteria.com>
2019-02-11 11:02:30 +0100

    in memory storage webapi

---
+++ core_webapi/Models/CityDTO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace core_webapi.Models
+{
+    public class CityDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int NumberOfPointOfInterest { get
+            {
+                return PointsOfInterest.Count;
+            }
+        }
+        public ICollection<PointOfInterestDto> PointsOfInterest { get; set; } = new List<PointOfInterestDto>();
+    }
+}
---
+++ core_webapi/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.IO;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+
+namespace core_webapi
+{
+    public class Program
+    {
+        public static void Main(string[] args) => CreateWebHostBuilder(args).Build().Run();
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+    }
+}
